87af5046ee71a5dd0e437ab9419bc3efd13b9f56
WPZ <wu_gy@mail.ustc.edu.cn>
2024-10-05 09:11:43 +0800

    [add] : search box

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchIcon = document.getElementById('searchIcon');
-    const searchInput = document.querySelector('.search-input');
+let inputBox = document.querySelector(".input-box"),
+                searchIcon = document.querySelector(".icon"),
+                closeIcon = document.querySelector(".close-icon");
 
-    searchIcon.addEventListener('click', function() {
-        searchInput.classList.toggle('active');
-        searchIcon.classList.toggle('active');
-        
-        if (searchInput.classList.contains('active')) {
-            searchInput.focus(); // 聚焦输入框
-        } else {
-            searchInput.blur(); // 失去焦点
-        }
-    });
-
-    // 点击输入框以外的区域时，关闭输入框
-    document.addEventListener('click', function(event) {
-        if (!searchContainer.contains(event.target)) {
-            searchInput.classList.remove('active');
-            searchIcon.classList.remove('active');
-        }
-    });
-});
+            searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
+            closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
